b187e0de4a8c056988a5b67269adf9873d4c86cc
xiexinch <test767803@foxmail.com>
2022-02-25 19:27:04 +0800

    add roundganv3

--- model/losses/__init__.py
+++ model/losses/__init__.py
@@ -1,5 +1,6 @@
 from .adv_loss import least_square_loss
 from .static_loss import StaticLoss
 from .cx_loss import ContexturalLoss, ContextualBilateralLoss
+from .perceptual_loss import PerceptualLoss
 
-__all__ = ['least_square_loss', 'StaticLoss', 'ContexturalLoss', 'ContextualBilateralLoss']
+__all__ = ['least_square_loss', 'StaticLoss', 'ContexturalLoss', 'ContextualBilateralLoss', 'PerceptualLoss']
